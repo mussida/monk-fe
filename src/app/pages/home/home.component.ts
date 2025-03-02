dba072a5f8412b03b7a771b3c23a3d0dafa03170
mussida <96059227+mussida@users.noreply.github.com>
2025-03-02 23:31:57 +0100

    feat: ad rules

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-import { UserStandingComponent } from '../../components/user-standing/user-standing.component';
-import { TitleComponent } from '../../components/title/title.component';
-import { TeamsStandingComponent } from '../../components/teams-standing/teams-standing.component';
-import { StandingPreviewsComponent } from '../../components/standing-previews/standing-previews.component';
-import { HomeImageComponent } from '../../components/home-image/home-image.component';
 import { CompetitionSectionComponent } from '../../components/competition-section/competition-section.component';
+import { HomeImageComponent } from '../../components/home-image/home-image.component';
+import { StandingPreviewsComponent } from '../../components/standing-previews/standing-previews.component';
 
 @Component({
-    selector: 'app-home',
-    imports: [
-        UserStandingComponent,
-        TitleComponent,
-        TeamsStandingComponent,
-        StandingPreviewsComponent,
-        HomeImageComponent,
-        CompetitionSectionComponent,
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css'
+  selector: 'app-home',
+  imports: [
+    StandingPreviewsComponent,
+    HomeImageComponent,
+    CompetitionSectionComponent,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {}
--- src/app/pages/rules/rules.component.ts
+++ src/app/pages/rules/rules.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-rules',
-    imports: [],
-    templateUrl: './rules.component.html',
-    styleUrl: './rules.component.css'
+  selector: 'app-rules',
+  imports: [],
+  templateUrl: './rules.component.html',
+  styleUrl: './rules.component.css',
 })
 export class RulesComponent {
-
+  routesPoints = [
+    {
+      grade: '5A',
+      points: 10,
+    },
+    {
+      grade: '5B',
+      points: 25,
+    },
+    {
+      grade: '5C',
+      points: 40,
+    },
+    {
+      grade: '6A',
+      points: 60,
+    },
+    {
+      grade: '6A+',
+      points: 75,
+    },
+    {
+      grade: '6B',
+      points: 90,
+    },
+    {
+      grade: '6B+',
+      points: 105,
+    },
+    {
+      grade: '6C',
+      points: 120,
+    },
+    {
+      grade: '6C+',
+      points: 135,
+    },
+    {
+      grade: '7A',
+      points: 160,
+    },
+    {
+      grade: '7A+',
+      points: 185,
+    },
+    {
+      grade: '7B',
+      points: 200,
+    },
+  ];
 }
