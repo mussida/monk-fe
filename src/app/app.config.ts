c318a4376f0c8d89b32c006f86de6b5c78a04c22
Youssef Awni Barty Fahmi Hanna <96059227+mussida@users.noreply.github.com>
2025-03-03 14:53:07 +0100

    fix: remove useless providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 import { routes } from './app.routes';
@@ -11,6 +10,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
 };
