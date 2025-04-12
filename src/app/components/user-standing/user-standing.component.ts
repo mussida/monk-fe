321c696cbebfb9a32cfaa577b33708c683a3b01d
mussida <96059227+mussida@users.noreply.github.com>
2025-04-12 17:09:30 +0200

    feat: add concat between carousels

--- src/app/components/user-standing/user-standing.component.ts
+++ src/app/components/user-standing/user-standing.component.ts
@@ -1,19 +1,32 @@
 import { Component, model } from '@angular/core';
-import { User } from '../../services/data-service.service';
+import { Team, User } from '../../services/data-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-standing',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-standing.component.html',
   styleUrl: './user-standing.component.css',
 })
 export class UserStandingComponent {
   users = model<User[]>([]);
+  teams = model<Team[]>([]); // Aggiungi le squadre
   showAlert = false;
 
   ngOnInit(): void {
     this.users.update((users) =>
       [...users].sort((a, b) => b.points - a.points)
     );
+  }
+
+  // Metodo per ottenere la squadra di un utente
+  getTeam(teamId: number): Team | undefined {
+    console.log('teamId', teamId);
+    return this.teams().find((team) => team.id === teamId);
+  }
+
+  getTeamName(teamId: number): string {
+    const team = this.getTeam(teamId);
+    return team ? team.name : 'Nessuna squadra';
   }
 }
