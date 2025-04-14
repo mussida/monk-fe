990ff725b7e749935fa255592adc9dec41badd68
mussida <96059227+mussida@users.noreply.github.com>
2025-04-14 14:36:49 +0200

    feat: add concat between carousels

--- src/app/pages/rules/rules.component.ts
+++ src/app/pages/rules/rules.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rules',
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterLink],
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.css',
 })
